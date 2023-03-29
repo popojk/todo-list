b3d39bd4e0c667f6c1568588ee1d25afbe3ccdc2
popojk <c.alexwu@gmail.com>
2023-03-29 21:27:16 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
-
 const exphbs = require('express-handlebars')
+
 //request body parser
 app.use(express.urlencoded({ extended: true }))
+//定義要使用的樣版引擎
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+//告訴express要使用的view engine 是 handlebar
+app.set('view engine', 'hbs')
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -12,8 +16,6 @@ if (process.env.NODE_ENV !== 'production') {
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-
 
 //取得資料庫連線狀態
 const db = mongoose.connection
@@ -28,17 +30,11 @@ db.once('open', () =>{
 
 const port = 3000
 
-//定義要使用的樣版引擎
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-
-//告訴express要使用的view engine 是 handlebar
-app.set('view engine', 'handlebars')
-
 //設定靜態檔案路由，讓伺服器知道靜態檔案要去哪邊查找
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('todo list')
+  res.render('index')
 })
 
 app.listen(port, () => {
