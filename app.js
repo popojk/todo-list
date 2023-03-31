432750090477f0643353fcf9e4a87ff34a2bab84
popojk <c.alexwu@gmail.com>
2023-03-31 22:40:46 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   Todo.find() //取出Todo model所有資料
     .lean() //把mongoose的model物件轉換為乾淨的JS資料陣列
+    .sort({ _id: 'asc' })
     .then(todos => res.render('index', { todos }))  //將資料傳給前端樣板
     .catch(error => console.log(error)) //錯誤處理
 })
