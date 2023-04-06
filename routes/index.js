82c29a85e59bcdfa933ace4ba97852d434c4ff09
popojk <c.alexwu@gmail.com>
2023-04-06 20:41:43 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組
 router.use('/todos', todos)
+// 將網址結構符合 /users 字串開頭的 request 導向 users 模組
+router.use('/users', users)
 // 準備引入路由模組
 // 匯出路由器
 module.exports = router
